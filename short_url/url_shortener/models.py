1f390f1f32ddf90ac26b81702854b371e8720d02
Andy Dai <daikeren@gmail.com>
2013-01-27 00:31:23 +0800

    Begin work on models

--- short_url/url_shortener/models.py
+++ short_url/url_shortener/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Link(models.Model):
+    class Meta:
+        verbose_name = 'Link'
+        verbose_name_plural = 'Links'
+
+    def __unicode__(self):
+        pass
+
+    clicks = models.IntegerField(default=0)
+    url = models.URLField(blank=True)
+    code = models.CharField(default="", max_length=10)
